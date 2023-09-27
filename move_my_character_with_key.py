40c818c6070a28b7a5f9fd5e13e35773b9798af2
Minjae <flower14288@gmail.com>
2023-09-27 20:38:27 +0900

    화면 경계 구현, 이미지 조정

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -19,16 +19,20 @@ def handle_events():
             moving = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                char_x += 10
+                if char_x < 1280:
+                    char_x += 10
                 dir = 0
             elif event.key == SDLK_LEFT:
-                char_x -= 10
+                if char_x > 10:
+                    char_x -= 10
                 dir = 600
             elif event.key == SDLK_UP:
-                char_y += 10
+                if char_y < 1000:
+                    char_y += 10
                 dir = 1200
             elif event.key == SDLK_DOWN:
-                char_y -= 10
+                if char_y > 50:
+                    char_y -= 10
                 dir = 1800
             elif event.key == SDLK_ESCAPE:
                 moving = False
@@ -40,7 +44,7 @@ frame = 0
 while moving:
     clear_canvas()
     back.draw_now(640, 512)
-    character.clip_draw(frame * 100, dir, 600, 400, char_x, char_y, 150, 100)
+    character.clip_draw(frame * 100, dir, 800, 550, char_x, char_y, 150, 100)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
